86bf5c242992892ee070456a09bce3bea5e241f6
ROHAN PARIJA <172002rohanparija@gmail.com>
2026-06-10 12:10:36 +0530

    Support dynamic port binding for hosting platforms like Hugging Face

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+import os
 import uvicorn
 
 from app.config import get_settings
@@ -5,4 +6,6 @@ from app.config import get_settings
 
 if __name__ == "__main__":
     settings = get_settings()
-    uvicorn.run("app.main:app", host=settings.api_host, port=settings.api_port, reload=settings.app_env == "development")
+    port = int(os.environ.get("PORT", settings.api_port))
+    uvicorn.run("app.main:app", host=settings.api_host, port=port, reload=settings.app_env == "development")
+
